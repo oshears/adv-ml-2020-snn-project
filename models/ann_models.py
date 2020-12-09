5cbcaed04ea021657716a1ce434684dac7f6dd80
Ahmadhossein Yazdani <34987996+Ayazdani1997@users.noreply.github.com>
2020-12-08 19:18:15 -0500

    Update ann_models.py
    Comments added to ann_models.py

--- models/ann_models.py
+++ models/ann_models.py
@@ -1,20 +1,33 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+## The network we use for ANN benchmark. This network has two layers like what follows:
+# layer1: input_size: 28 * 28 (The width and height of MNIST examples), output_size: 100
+# layer2: input_size: 100, output_size: 10
+# First layer applies Relu activation function on the linear combination of the inputs, while second layer does not apply any activation function.
+# That's because pytorch negative log likelihood loss function itself applies softmax.
+#
+#
 class ANN_Model(nn.Module):
-    def __init__(self, class_num, act=F.relu):
+    ## initializing the network like mentioned above.
+    def __init__(self, act=F.relu):
         super(ANN_Model, self).__init__()
 
 
         self.layer1 = nn.Linear(1 * 28 * 28, 1 * 100)
-        self.act1 = act
+        self.act = act
+        
+        self.layer2 = nn.Linear(100, 10)
 
-
+    ## this method is called while we feed the data into the network in the forward pass.
     def forward(self, x):
 
         x = x.view(x.size(0), -1)
 
         x = self.layer1(x)
-        x = self.act1(x)
+        x = self.act(x)
+        
+        x = self.layer2(x)
 
         return x
